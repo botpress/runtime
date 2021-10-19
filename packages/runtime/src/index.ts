37fddbde7f684ab54c0fcb580960f288497b0780
allardy <yann.allard1@gmail.com>
2021-10-19 14:27:51 -0400

    chore(runtime): re-added migrations

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -95,10 +95,20 @@ try {
           description: 'Whether you want to run in production mode or not',
           default: false,
           type: 'boolean'
+        },
+        autoMigrate: {
+          description:
+            'When this flag is set, Botpress will automatically migrate your content and configuration files when upgrading',
+          default: false,
+          type: 'boolean'
         }
       },
       async argv => {
         process.IS_PRODUCTION = argv.production || yn(process.env.BP_PRODUCTION) || yn(process.env.CLUSTER_ENABLED)
+
+        process.AUTO_MIGRATE =
+          process.env.AUTO_MIGRATE === undefined ? yn(argv.autoMigrate) : yn(process.env.AUTO_MIGRATE)
+
         process.VERBOSITY_LEVEL = argv.verbose ? Number(argv.verbose) : defaultVerbosity
         process.distro = await getos()
 
