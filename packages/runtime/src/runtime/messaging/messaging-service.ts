703f70737b00442aa8b67b44526a41d2337783a7
Samuel Massé <samuelmasse4@gmail.com>
2021-10-29 12:55:18 -0400

    bring changes

--- packages/runtime/src/runtime/messaging/messaging-service.ts
+++ packages/runtime/src/runtime/messaging/messaging-service.ts
@@ -1,4 +1,4 @@
-import { MessagingClient } from '@botpress/messaging-client'
+import { MessagingClient, uuid } from '@botpress/messaging-client'
 import { IO, Logger, MessagingConfig } from 'botpress/runtime-sdk'
 import { formatUrl, isBpUrl } from 'common/url'
 import { inject, injectable, postConstruct } from 'inversify'
@@ -17,8 +17,7 @@ export class MessagingService {
   private webhookTokenByClientId: { [botId: string]: string } = {}
   private channelNames = ['messenger', 'slack', 'smooch', 'teams', 'telegram', 'twilio', 'vonage', 'messaging']
   private newUsers: number = 0
-  private eventIdToMessageIdCache: LRUCache<string, string>
-  private collectingForMessageCache: LRUCache<string, boolean>
+  private collectingCache: LRUCache<string, uuid>
 
   public isExternal: boolean
   public internalPassword: string | undefined
@@ -29,8 +28,7 @@ export class MessagingService {
     @inject(TYPES.Logger) private logger: Logger
   ) {
     this.isExternal = Boolean(process.core_env.MESSAGING_ENDPOINT)
-    this.eventIdToMessageIdCache = new LRUCache<string, string>({ max: 5000, maxAge: ms('5m') })
-    this.collectingForMessageCache = new LRUCache<string, boolean>({ max: 5000, maxAge: ms('5m') })
+    this.collectingCache = new LRUCache<string, uuid>({ max: 5000, maxAge: ms('5m') })
   }
 
   async initialize() {
@@ -117,9 +115,8 @@ export class MessagingService {
       botId: this.botsByClientId[msg.clientId]
     })
 
-    this.eventIdToMessageIdCache.set(event.id, msg.message.id)
     if (msg.collect) {
-      this.collectingForMessageCache.set(msg.message.id, true)
+      this.collectingCache.set(event.id, msg.message.id)
     }
 
     return this.eventEngine.sendEvent(event)
@@ -132,11 +129,12 @@ export class MessagingService {
 
     const payloadAbsoluteUrl = this.convertToAbsoluteUrls(event.payload)
 
+    const collecting = event.incomingEventId && this.collectingCache.get(event.incomingEventId)
     const message = await this.clientsByBotId[event.botId].messages.create(
       event.threadId!,
       undefined,
       payloadAbsoluteUrl,
-      { incomingId: this.eventIdToMessageIdCache.get(event.incomingEventId!)! }
+      collecting ? { incomingId: collecting } : undefined
     )
     event.messageId = message.id
 
@@ -144,7 +142,7 @@ export class MessagingService {
   }
 
   public informProcessingDone(event: IO.IncomingEvent) {
-    if (this.collectingForMessageCache.get(event.messageId!)) {
+    if (this.collectingCache.get(event.messageId!)) {
       // We don't want the waiting for the queue to be empty to freeze other messages
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
       this.sendProcessingDone(event)
@@ -155,6 +153,7 @@ export class MessagingService {
     try {
       await this.eventEngine.waitOutgoingQueueEmpty(event)
       await this.clientsByBotId[event.botId].messages.endTurn(event.messageId!)
+      this.collectingCache.del(event.messageId!)
     } catch (e) {
       this.logger.attachError(e).error('Failed to inform messaging of completed processing')
     }
